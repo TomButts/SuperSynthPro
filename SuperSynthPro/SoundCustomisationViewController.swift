7b691d955470cffa589828e5fd9bffd8e20dce75
Tom Butts <tb789@york.ac.uk>
2016-12-15 00:04:07 +0000

    Dials mother fuckerrrrr

--- SuperSynthPro/SoundCustomisationViewController.swift
+++ SuperSynthPro/SoundCustomisationViewController.swift
@@ -13,12 +13,42 @@ class SoundCustomisationViewController: UIViewController {
 
     @IBOutlet var startStopSwitch: UISwitch!
     
-    enum ControlTag: Int {
-        case attackKnob = 1
-    }
+    @IBOutlet var attackKnobPlaceholder: UIView!
+    @IBOutlet var decayKnobPlaceholder: UIView!
+    @IBOutlet var sustainKnobPlaceholder: UIView!
+    @IBOutlet var releaseKnobPlaceholder: UIView!
+    
+    @IBOutlet weak var attackLabel: UILabel!
+    @IBOutlet weak var decayLabel: UILabel!
+    @IBOutlet weak var sustainLabel: UILabel!
+    @IBOutlet weak var releaseLabel: UILabel!
+    
+    var attackKnob: Knob!
+    var decayKnob: Knob!
+    var sustainKnob: Knob!
+    var releaseKnob: Knob!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // ADSR
+        attackKnob = Knob(frame: attackKnobPlaceholder.bounds)
+        attackKnob.addTarget(self, action: #selector(SoundCustomisationViewController.attackValueChanged), for: .valueChanged)
+        attackKnobPlaceholder.addSubview(attackKnob)
+        
+        decayKnob = Knob(frame: decayKnobPlaceholder.bounds)
+        decayKnob.addTarget(self, action: #selector(SoundCustomisationViewController.decayValueChanged), for: .valueChanged)
+        decayKnobPlaceholder.addSubview(decayKnob)
+    
+        sustainKnob = Knob(frame: sustainKnobPlaceholder.bounds)
+        sustainKnob.addTarget(self, action: #selector(SoundCustomisationViewController.sustainValueChanged), for: .valueChanged)
+        sustainKnobPlaceholder.addSubview(sustainKnob)
+        
+        releaseKnob = Knob(frame: releaseKnobPlaceholder.bounds)
+        releaseKnob.addTarget(self, action: #selector(SoundCustomisationViewController.releaseValueChanged), for: .valueChanged)
+        releaseKnobPlaceholder.addSubview(releaseKnob)
+        
+        view.tintColor = UIColor.blue
         
         let placeholder = AKOscillator(waveform: AKTable(.sine))
         let plotFrame = CGRect(x: 100.0, y: 100.0, width: 820.0, height: 220.0)
@@ -50,6 +80,11 @@ class SoundCustomisationViewController: UIViewController {
         
         adsr = ADSREnvelope(node: generator.waveNode)
         
+        attackKnob.value = Float(adsr.envelope.attackDuration)
+        decayKnob.value = Float(adsr.envelope.decayDuration)
+        sustainKnob.value = Float(adsr.envelope.sustainLevel)
+        releaseKnob.value = Float(adsr.envelope.releaseDuration)
+        
         AudioKit.output = adsr.envelope
         
         plot.node = adsr.envelope
@@ -57,12 +92,6 @@ class SoundCustomisationViewController: UIViewController {
         AudioKit.start()
         
         adsr.envelope.stop()
-        
-    }
-    
-    func setupUI() {
-        // draw envelope element
-        
     }
     
     @IBAction func startStopSwitchValueChanged(_ sender: UISwitch) {
@@ -74,4 +103,25 @@ class SoundCustomisationViewController: UIViewController {
             startStopSwitch.setOn(false, animated: true)
         }
     }
+
+    func attackValueChanged() {
+        adsr.envelope.attackDuration = Double(attackKnob.value)
+        attackLabel.text = String(Double(adsr.envelope.attackDuration))
+    }
+    
+    func decayValueChanged() {
+        adsr.envelope.decayDuration = Double(decayKnob.value)
+        decayLabel.text = String(Double(adsr.envelope.decayDuration))
+    }
+    
+    func sustainValueChanged() {
+        adsr.envelope.sustainLevel = Double(sustainKnob.value)
+        sustainLabel.text = String(Double(adsr.envelope.sustainLevel))
+    }
+    
+    func releaseValueChanged() {
+        adsr.envelope.releaseDuration = Double(releaseKnob.value)
+        releaseLabel.text = String(Double(adsr.envelope.releaseDuration))
+    }
+    
 }
